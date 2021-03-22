f164ff93180d22f97d0f0584e4c5d1eefd150a3b
Adriel Isaiah Amoguis <adriel_amoguis@dlsu.edu.ph>
2021-03-22 15:35:37 +0800

    Index Page Complete
    Reset the register page

--- src/sources/components/ButtonLink.js
+++ src/sources/components/ButtonLink.js
@@ -1,9 +1,20 @@
-import '../assets/styles.css'
+import "../assets/styles.css";
+import { Link } from "react-router-dom";
 
 export default function Button(props) {
+    // Check if to was given or href
+
+    if (props.to !== undefined) {
+        return (
+            <Link className="button" to={props.to} style={props.style}>
+                <center>{props.text}</center>
+            </Link>
+        );
+    }
+
     return (
         <a className="button" href={props.href} style={props.style}>
             <center>{props.text}</center>
         </a>
-    )
+    );
 }
