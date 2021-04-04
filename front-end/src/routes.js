852391ff84814d1b1c0004ac11ae9566441241a5
Lorenzo Querol <renzo_querol@dlsu.edu.ph>
2021-04-04 13:41:29 +0800

    Created new protected route for "My Appointments" webpage
    Added a protected route for the "My Appointments" webpage

--- front-end/src/routes.js
+++ front-end/src/routes.js
@@ -1,40 +1,41 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import ProtectedRoute from "./sources/components/ProtectedRoute";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ProtectedRoute from './sources/components/ProtectedRoute';
 
 // Route Imports
-import NavHeader from "./sources/components/NavHeader";
-import Footer from "./sources/components/Footer";
-import Homepage from "./sources/Homepage";
-import Register from "./sources/Register";
-import Login from "./sources/Login";
-import PageNotFound from "./sources/components/PageNotFound";
+import NavHeader from './sources/components/NavHeader';
+import Footer from './sources/components/Footer';
+import Homepage from './sources/Homepage';
+import Register from './sources/Register';
+import Login from './sources/Login';
+import PageNotFound from './sources/components/PageNotFound';
 
 // Protected Route Imports
-import Dashboard from "./sources/UserDashboard";
-import MyCalendar from "./sources/MyCalendar";
-import Profile from "./sources/Profile";
+import Dashboard from './sources/UserDashboard';
+import MyCalendar from './sources/MyCalendar';
+import MyAppointments from './sources/MyAppointments';
+import Profile from './sources/Profile';
 
 // Material UI
-import { createMuiTheme, responsiveFontSizes } from "@material-ui/core/styles";
-import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 var theme = createMuiTheme({
     palette: {
         primary: {
-            main: "#7868E6",
+            main: '#7868E6',
         },
         secondary: {
-            main: "#B8B5FF",
+            main: '#B8B5FF',
         },
         accent: {
-            main: "#EDEEF7",
+            main: '#EDEEF7',
         },
         dark: {
-            main: "#212121",
+            main: '#212121',
         },
         complement: {
-            main: "#E4FBFF",
+            main: '#E4FBFF',
         },
     },
 });
@@ -49,39 +50,16 @@ export default function Routes(props) {
 
                 <Switch>
                     {/* PUBLIC ROUTES */}
-                    <ProtectedRoute
-                        exact
-                        path="/"
-                        component={Homepage}
-                        protected={false}
-                    />
-                    <ProtectedRoute
-                        path="/register"
-                        component={Register}
-                        protected={false}
-                    />
-                    <ProtectedRoute
-                        path="/login"
-                        component={Login}
-                        protected={false}
-                    />
+                    <ProtectedRoute exact path="/" component={Homepage} protected={false} />
+                    <ProtectedRoute path="/register" component={Register} protected={false} />
+                    <ProtectedRoute path="/login" component={Login} protected={false} />
 
                     {/* PROTECTED ROUTES */}
-                    <ProtectedRoute
-                        path="/dashboard"
-                        component={Dashboard}
-                        protected={true}
-                    />
-                    <ProtectedRoute
-                        path="/my-calendar"
-                        component={MyCalendar}
-                        protected={true}
-                    />
-                    <ProtectedRoute
-                        path="/profile"
-                        component={Profile}
-                        protected={true}
-                    />
+                    <ProtectedRoute path="/dashboard" component={Dashboard} protected={true} />
+                    <ProtectedRoute path="/my-calendar" component={MyCalendar} protected={true} />
+                    <ProtectedRoute path="/my-appointments" component={MyAppointments} protected={true} />
+                    <ProtectedRoute path="/profile" component={Profile} protected={true} />
+
                     {/* 404 */}
                     <Route component={PageNotFound} />
                 </Switch>
