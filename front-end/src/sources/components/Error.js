a2f99f996874dfea484ef913d8cd6bcb166bfdf7
gianissimo0829 <gjbm.exe@gmail.com>
2021-04-04 14:27:07 +0800

    Initial Commit

---
+++ front-end/src/sources/components/Error.js
@@ -0,0 +1,10 @@
+import "./assets/styles.css";
+
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function Profile() {
+    return(
+        
+    )
+}
