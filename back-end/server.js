d17c7b28258bb1598a31a7831ce1243dfbb7d836
Adriel Isaiah Amoguis <adriel_amoguis@dlsu.edu.ph>
2021-04-26 06:58:45 +0800

    More deployment fixes

--- back-end/server.js
+++ back-end/server.js
@@ -69,8 +69,8 @@ const sessionModel = {
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
     cookie: {
-        secure: process.env.PRODUCTION === 1 ? "true" : "false",
-        sameSite: process.env.PRODUCTION === 1 ? "none" : "lax",
+        secure: Number(process.env.PRODUCTION) === 1,
+        sameSite: Number(process.env.PRODUCTION) === 1 ? "none" : "lax",
         httpOnly: true,
         maxAge: Number(process.env.MAX_COOKIE_AGE) || 1e6,
     },
