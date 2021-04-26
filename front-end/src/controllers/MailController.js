34deefccf0493b09a3f064773125a96382a56bc9
Adriel Isaiah Amoguis <adriel_amoguis@dlsu.edu.ph>
2021-04-27 03:58:19 +0800

    Implemented getInbox and getSentbox
    There is an issue with the React-Lifecycle that sometimes messes with the mail loading, causing an object undefined error.

--- front-end/src/controllers/MailController.js
+++ front-end/src/controllers/MailController.js
@@ -1,74 +1,47 @@
-// Import the Placeholder File
-import mailDB from '../placeholderData/mail';
+import request from "../utils/AxiosConfig";
 
-import { GetUserData } from './UserController';
+import { GetUserData } from "./UserController";
 
-export const getInbox = async (uid, start = 0, end = 50) => {
-    // Normally, we make an API call here.
+export const getInbox = async (start = 0, end = 50) => {
+    // Make API Call
+    try {
+        const response = await request.get("api/mail/inbox");
 
-    // For Phase 1:
-    const allMail = mailDB.filter((mail) => mail.recepientID === Number(uid));
-
-    // Get sender details
-    const selected = allMail.slice(start, end);
-
-    selected.map(async (mail) => {
-        // Get the sender details
-        mail.sender = await GetUserData(mail.senderID);
-
-        // Parse the time
-        const date = new Date(mail.sendTime);
-        const now = new Date();
-
-        if (
-            date.getDate() === now.getDate() &&
-            date.getMonth() === now.getMonth() &&
-            date.getFullYear() === now.getFullYear()
-        ) {
-            // Parse as time
-            mail.sendTime = `Today ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        if (response.status === 200) {
+            // Add user data for recepient & format dates
+            const mailObject = response.data;
+            await mailObject.map(async m => {
+                m.sender = await GetUserData(m.senderID);
+                m.sendTime = new Date(m.sendTime).toLocaleString().toUpperCase();
+            });
+            return mailObject;
         }
-        // Parse as datetime
-        else mail.sendTime = date.toDateString();
-    });
-
-    // Sort by date
-
-    return selected;
+        return false;
+    } catch (ex) {
+        console.error(ex);
+        return false;
+    }
 };
 
-export const getSent = async (uid, start = 0, end = 50) => {
-    // Normally, we make an API call here.
+export const getSent = async (start = 0, end = 50) => {
+    // Make API Call
+    try {
+        const response = await request.get("api/mail/sentbox");
 
-    // For Phase 1:
-    const allMail = mailDB.filter((mail) => mail.senderID === Number(uid));
-
-    // Get sender details
-    const selected = allMail.slice(start, end);
-
-    selected.map(async (mail) => {
-        // Get the sender details
-        mail.recepient = await GetUserData(mail.recepientID);
-
-        // Parse the time
-        const date = new Date(mail.sendTime);
-        const now = new Date();
-
-        if (
-            date.getDate() === now.getDate() &&
-            date.getMonth() === now.getMonth() &&
-            date.getFullYear() === now.getFullYear()
-        ) {
-            // Parse as time
-            mail.sendTime = `Today ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        if (response.status === 200) {
+            // Add user data for recepient & format dates
+            const mailObject = response.data;
+            await mailObject.map(async m => {
+                m.recepient = await GetUserData(m.recepientID);
+                m.sendTime = new Date(m.sendTime).toLocaleString().toUpperCase();
+            });
+            return mailObject;
         }
-        // Parse as datetime
-        else mail.sendTime = date.toDateString();
-    });
-
-    // Sort by date
-
-    return selected;
+        return false;
+    } catch (ex) {
+        console.error(ex);
+        return false;
+    }
 };
 
 export const sendMessage = (userEmail, messageContent) => {
